5790a711246e4994e960d8e6ce399f7d2d7d300d
yarikrik <solodovnikovaroslav41@gmail.com>
2026-09-21 13:28:37 +0300

    joles

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-#include <iostream>
-
 int addNumbers(int a, int b) {
     return a + b;
-}
-
-int main() {
-    int result = addNumbers(5, 7);
-    std::cout << "Сумма: " << result << std::endl; 
-    return 0;
 }
